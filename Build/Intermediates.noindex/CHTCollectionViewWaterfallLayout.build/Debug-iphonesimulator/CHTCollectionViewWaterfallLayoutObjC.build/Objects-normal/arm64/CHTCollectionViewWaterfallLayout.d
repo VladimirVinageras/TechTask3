74030abea907f6c4c217e56a535f392f4532c5db
Vladimir Vinageras <vvinageras@gmail.com>
2024-11-07 11:32:01 +0200

    commit

--- Build/Intermediates.noindex/CHTCollectionViewWaterfallLayout.build/Debug-iphonesimulator/CHTCollectionViewWaterfallLayoutObjC.build/Objects-normal/arm64/CHTCollectionViewWaterfallLayout.d
+++
@@ -1,5 +0,0 @@
-dependencies: \
-  /Users/vinakheras/Library/Developer/Xcode/DerivedData/TechTask3-gfijkyzeupesksfeapeljfoxnykk/SourcePackages/checkouts/CHTCollectionViewWaterfallLayout/Source/CHTCollectionViewWaterfallLayout.m \
-  /Users/vinakheras/Library/Developer/Xcode/DerivedData/TechTask3-gfijkyzeupesksfeapeljfoxnykk/SourcePackages/checkouts/CHTCollectionViewWaterfallLayout/Source/CHTCollectionViewWaterfallLayout.h \
-  /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/System/Library/Frameworks/UIKit.framework/Modules/module.modulemap \
-  /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/16/include/module.modulemap
